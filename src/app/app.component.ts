fd844ba8ac425dfbcae3be958df8d0e568c30540
Poojethaa Kumar <poojethaa.kumar2@mindtree.com>
2021-01-11 19:35:20 +0530

    jenkins test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
+//ngrx
+import {Store} from '@ngrx/store';
+import {Observable}  from 'rxjs';
 
+import {Post} from './post.model';
+import * as PostActions from './post.actions'
+interface AppState{
+  //message:string;
+  post:Post
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +16,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngrx-demo';
+  //message$:Observable<string>
+  post$:Observable<Post>
+  text:string
+  constructor(private store:Store<AppState>){
+    // this.message$=this.store.select('message');
+    this.post$=this.store.select('post')
+  }
+   editText(){
+     this.store.dispatch(new PostActions.EditText(this.text))
+   }
+   resetPost(){
+     this.store.dispatch(new PostActions.Reset())
+   }
+   upvote(){
+    this.store.dispatch(new PostActions.Upvote());
+   }
+   downvote(){
+    this.store.dispatch(new PostActions.Downvote());
+   }
+  spanishMessage(){
+    this.store.dispatch({type:'SPANISH'});
+
+  }
+  frenchMessage(){
+    this.store.dispatch({type:'FRENCH'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+//ngrx
+import {StoreModule} from '@ngrx/store';
+import {simpleReducer} from './simple.reducer';
+import {postReducer} from './reducers/post.reducer';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    StoreModule.forRoot({
+      post:postReducer,
+     // message:simpleReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
